dc58b231d9c085d9c07f3f8151cf5b98866c464c
birbyblaster <s3916032@student.rmit.edu.au>
2021-10-17 19:54:01 +1100

    main_app was being called despite not being a class

--- kivy-main/main.py
+++ kivy-main/main.py
@@ -37,16 +37,8 @@
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.lang import Builder
 from kivy.properties import ObjectProperty
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.screenmanager import *
-from kivy.uix.textinput import TextInput
-from kivy.uix.image import Image
-from kivy.uix.widget import Widget
 from kivy.core.window import Window
 import os
 from random import random
@@ -631,4 +623,4 @@ def check_integrity():
 
 if __name__ == "__main__":
     main_app = MainApp()  # build and then run the test app
-    main_app().run()
+    main_app.run()
